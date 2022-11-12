79abc9df60142432a734e01e202fbdb8ee4d9cca
Jo <matsu@Joannes-Air-2.lan>
2022-11-12 01:13:14 +0000

    Improved code

--- ASCII.py
+++ ASCII.py
@@ -1,10 +1,10 @@
 #Name: Joanne Alcock
 #Fun fact about me: I completed this in 4 different ways but as I'm a complete beginner I have no idea what is considered the most efficient way lol.
 #This is a Codecademy project from the module Learn Python 3, Project: Block Letters.
-print("JJJJJ" + "  A  ")
-print("  J  " + " A A ")
-print("  J  " + "A   A")
-print("  J  " + "AAAAA")
-print("J J  " + "A   A")
-print("J J  " + "A   A")
-print(" JJ  " + "A   A")
+print("JJJJJ   A  ")
+print("  J    A A ")
+print("  J   A   A")
+print("  J   AAAAA")
+print("J J   A   A")
+print("J J   A   A")
+print(" JJ   A   A")
